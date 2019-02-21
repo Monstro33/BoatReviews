0ba01e0e679b4dd2b1029fd1df18d2c870c47611
Michael Manville <monstro33@yahoo.com>
2019-02-21 12:17:05 -0500

    Added 1 Test

---
+++ BoatReviews.Tests/BoatControllerTests.cs
@@ -0,0 +1,26 @@
+using BoatReviews.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BoatReviews.Tests
+{
+    public class BoatControllerTests
+    {
+        public BoatController underTest;
+        public BoatControllerTests()
+        {
+            var underTest = new BoatController();
+        }
+    
+        [Fact]
+        public void Index_Returns_A_View_Result()
+        {
+            var result = underTest.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
