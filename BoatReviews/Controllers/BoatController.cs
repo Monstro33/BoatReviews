0bea84a24f233266b91ec19e8a4083d4437f0467
Joe Scheiman <scheiman.joe@gmail.com>
2019-02-22 16:02:14 -0500

    Re-Pushing to master, added CSS

---
+++ BoatReviews/Controllers/BoatController.cs
@@ -0,0 +1,32 @@
+﻿using BoatReviews.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoatReviews.Controllers
+{
+    public class BoatController : Controller
+    {
+        BoatRepository boatRepo;
+
+        public BoatController()
+        {
+            boatRepo = new BoatRepository();
+        }
+
+        public ViewResult Index()
+        {
+            var model = boatRepo.GetAll();
+            return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var model = boatRepo.GetById(id);
+            return View(model);
+        }
+    }
+}
+ 
---
+++ BoatReviews/Repositories/BoatRepository.cs
@@ -0,0 +1,28 @@
+﻿using BoatReviews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoatReviews.Repositories
+{
+    public class BoatRepository
+    {
+        public Dictionary<int, Boat> SomeBoats = new Dictionary<int, Boat>()
+        {
+            {1, new Boat() { Id = 1, Name = "Motorboat", ImageLocation = "Img/Motorboat1.jpeg" } },
+            {2, new Boat() { Id = 2, Name = "Sailboat", ImageLocation = "Img/Sailboat1.jpeg" } },
+            {3, new Boat() { Id = 3, Name = "Human Powered Boat", ImageLocation = "Img/HPB1.jpg" } }
+        };
+
+        public IEnumerable<Boat> GetAll()
+        {
+            return SomeBoats.Values.ToList();
+        }
+
+        public object GetById(int id)
+        {
+            return SomeBoats[id];
+        }
+    }
+}
