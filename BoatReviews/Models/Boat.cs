5af0d1c9b424b704fb3f13066983138af234e9df
Mary McGeary <malicemcgeary@gmail.com>
2019-02-25 09:55:31 -0500

    added details.cs and added list to boat.cs

--- BoatReviews/Models/Boat.cs
+++ BoatReviews/Models/Boat.cs
@@ -10,6 +10,40 @@ namespace BoatReviews.Models
         public string Name { get; set; }
         public int Id { get; set; }
         public string ImageLocation { get; set; }
+        public List<string> Reviews { get; set; }
+        public List<string> Images { get; set; }
+
+        private Details details = new Details();
+
+
+        public Boat(int id, string name, string imageLocation)
+        {
+            Name = name;
+            Id = id;
+            ImageLocation = imageLocation;
+
+            switch (Id)
+            {
+                case 1:
+                    Reviews = details.GetNitroReviews();
+                    Images = details.GetNitroImages();
+                    break;
+
+                case 2:
+                    Reviews = details.GetSeaRayReviews();
+                    Images = details.GetSeaRayImages();
+                    break;
+
+                case 3:
+                    Reviews = details.GetWellcraftReviews();
+                    Images = details.GetWellcraftImages();
+                    break;
+            }
+        }
+
+        public Boat()
+        {
+        }
     }
 
 }
