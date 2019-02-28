a7e2fa1ea2f92c9432652a0c23973fe440732586
Mary McGeary <malicemcgeary@gmail.com>
2019-02-28 14:47:03 -0500

    trying to pull master

---
+++ BoatReviews/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoatReviews.Models
+{
+    public class Category
+    {
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryImageLocation { get; set; }
+        public string CategoryContent { get; set; }
+    }
+}
