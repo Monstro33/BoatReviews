c3f1edfabd96254bfdb91852c84b85e684f563ce
Mary McGeary <malicemcgeary@gmail.com>
2019-03-07 12:00:14 -0500

    corrected seed data

--- BoatReviews/BoatContext.cs
+++ BoatReviews/BoatContext.cs
@@ -54,7 +54,7 @@ namespace BoatReviews
                 Id = 1,
                 CategoryId = 1,
                 Name = "Nitro",
-                ImageLocation = "~/Img/NitroBig.jpg",
+                ImageLocation = "Img/NitroBig.jpg",
                 ReviewContent = "There are lots of bass boats around, and we doubt that one catches fish better than the others – despite boat builders' claims. The fact is that the Nitro Z-6 has all of the fishing amenities and equipment needed to put tournament anglers on top of the bass without draining the last drop from their bank accounts. The Nitro Z-6 costs about $21 grand, including a trailer and Mercury 115-hp OptiMax outboard. The Nitro Z - 6 is a good choice for beginning boat owners, club tournament anglers or anybody wanting to chase fish on a budget.And in case you are wondering, it is one of the best - selling brands on the market."
             },
             new Boat()
@@ -62,7 +62,7 @@ namespace BoatReviews
                 Id = 2,
                 CategoryId = 1,
                 Name = "Sea Ray",
-                ImageLocation = "/Img/SeaRayBig.jpg",
+                ImageLocation = "Img/SeaRayBig.jpg",
                 ReviewContent = "Sea Ray has moved to fill a gap between its affordable “sport” lineup and the luxury SLX models and created a somewhat cost-conscious line that still embraces an upscale look. The result is the SPX Series. Affordable, but depending on how she gets optioned out, still embraces many of the amenities that the brand is well known for. Perhaps most importantly, the hulls are the same as her more expensive siblings, and for that reason we expect the ride and the handling to be on a par with them."
             },
             new Boat()
