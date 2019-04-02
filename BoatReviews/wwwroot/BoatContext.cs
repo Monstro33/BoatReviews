e0f465d240a7d919a934a816d1d9016e8a30a3b5
Michael Manville <monstro33@yahoo.com>
2019-04-02 13:10:05 -0400

    styled the create page

---
+++ BoatReviews/wwwroot/BoatContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoatReviews.wwwroot
+{
+    public class BoatContext : DbContext
+    {
+    
+    }
+}
